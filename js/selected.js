852e7cc17493a143f27e6251aa199b6d93fdcd97
sinha45 <alfa45leonidas@gmail.com>
2022-10-02 15:38:11 +0600

    input field modify

---
+++ js/selected.js
@@ -0,0 +1,6 @@
+document.getElementById('select-btn').addEventListener('click', function () {
+    const playerSelection = document.getElementById('player-selection');
+    const li = document.createElement('li');
+    li.innerText = 
+
+})
