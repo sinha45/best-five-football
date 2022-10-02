e3b81c7602e0316963a7dcb130754572471c75d5
sinha45 <alfa45leonidas@gmail.com>
2022-10-02 19:30:35 +0600

    box shadow added

--- js/selected.js
+++ js/selected.js
@@ -1,6 +1,37 @@
-document.getElementById('select-btn').addEventListener('click', function () {
-    const playerSelection = document.getElementById('player-selection');
-    const li = document.createElement('li');
-    li.innerText = 
+function playerList(playerName) {
 
-})
+    const playerText = playerName.innerText;
+
+    const olContainer = document.getElementById('player-selection');
+    const li = document.createElement('li');
+    li.innerText = playerText;
+    olContainer.appendChild(li);
+}
+
+
+document.getElementById('select-btn-1').addEventListener('click', function () {
+    const playerName = document.getElementById('player-name-1');
+    playerList(playerName);
+});
+
+document.getElementById('select-btn-2').addEventListener('click', function () {
+    const playerName = document.getElementById('player-name-2');
+    playerList(playerName);
+});
+
+document.getElementById('select-btn-3').addEventListener('click', function () {
+    const playerName = document.getElementById('player-name-3');
+    playerList(playerName);
+});
+document.getElementById('select-btn-4').addEventListener('click', function () {
+    const playerName = document.getElementById('player-name-4');
+    playerList(playerName);
+});
+document.getElementById('select-btn-5').addEventListener('click', function () {
+    const playerName = document.getElementById('player-name-5');
+    playerList(playerName);
+});
+document.getElementById('select-btn-6').addEventListener('click', function () {
+    const playerName = document.getElementById('player-name-6');
+    playerList(playerName);
+});
